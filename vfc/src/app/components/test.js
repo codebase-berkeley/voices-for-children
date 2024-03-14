16518ec1f0d9f7b42f178248cfe001ef8777af5f
Cindy Sanchez <cindysanchez@berkeley.edu>
2024-03-14 15:37:23 -0700

    test

--- vfc/src/app/components/test.js
+++ vfc/src/app/components/test.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-const Card = ({ frontContent, backContent }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
+// const Card = ({ frontContent, backContent }) => {
+//   const [isFlipped, setIsFlipped] = useState(false);
 
-  const handleClick = () => {
-    setIsFlipped(!isFlipped);
-  };
+//   const handleClick = () => {
+//     setIsFlipped(!isFlipped);
+//   };
 
-  return (
-    <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
-      <div className="front">{frontContent}</div>
-      <div className="back">{backContent}</div>
-    </div>
-  );
-};
+//   return (
+//     <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
+//       <div className="front">{frontContent}</div>
+//       <div className="back">{backContent}</div>
+//     </div>
+//   );
+// };
 
-export default Card;
+// export default Card;
