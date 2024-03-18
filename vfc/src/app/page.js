4e2d5672a744bd9d2bdfc244ae1e300dfa2f84a8
Justin Chen <justinchen722@berkeley.edu>
2024-03-17 18:52:30 -0700

    l

--- vfc/src/app/page.js
+++ vfc/src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Inventory from "./Components/inventory";
 import InventoryEntry from "./Components/inventoryentry";
+import Top from "./components/top";
 
 export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Inventory  />
+        <Top  />
     // </main>
   );
 }
