4139aacf1f029e06893771cd1ffe7d6ddce0d36d
saadhvi <saadhvinarayanan@berkeley.edu>
2024-03-17 20:06:15 -0700

    inv change

--- vfc/src/app/Components/inventory.js
+++ vfc/src/app/Components/inventory.js
@@ -9,43 +9,49 @@ import { useState } from 'react';
 function Inventory() {
     const [visible, setVisible] = useState(false);
     function show() {
-        setVisible(true);
+        setVisible(!visible);
         console.log(visible, "f")
     }
     return (
         <div className='inventory-page'>
             <div className = "search-wrapper">
-                <div className="search">
-                    <form><input type="text" placeholder="Search..." id="search"></input></form>
-                </div>
-                <div className="filter-wrappers">
-                    <div className="filter-by">
-                        <select name="filter-by" id="filter" placeholder="Filter By">
-                            <option value="">Filter By</option>
-                            <option value="sort">option1</option>
-                        </select>
+                <div className='filterContainer'>
+                    <div className="search">
+                        <form><input type="text" placeholder="Search..." id="search"></input></form>
                     </div>
-                    <div className="sort-by">
-                        <select name="sort-by" id="sort">
-                            <option value="">Sort By</option>
-                            <option value="sort">option1</option>
-                        </select>
+                    <div className="filter-wrappers">
+                        <div className="filter-by">
+                            <select name="filter-by" id="filter" placeholder="Filter By">
+                                <option value="">Filter By</option>
+                                <option value="sort">option1</option>
+                            </select>
+                        </div>
+                        <div className="sort-by">
+                            <select name="sort-by" id="sort">
+                                <option value="">Sort By</option>
+                                <option value="sort">option1</option>
+                            </select>
+                        </div>
+                            <button id="create-new" onClick={show}>Create New</button>
                     </div>
-                        <button id="create-new" onClick={show}>Create New</button>
-                        {visible && <EntryPopup />}
                 </div>
             </div>
-            <div className="inventory-wrapper">
-                <div className="inventory-header">
-                    <h2 className="inv-col-head">Donor</h2>
-                    <h2 className="inv-col-head">Items Donated</h2>
-                    <h2 className="inv-col-head">Item Type</h2>
-                    <h2 className="inv-col-head">Amount</h2>
-                    <h2 className="inv-col-head">Date Donated</h2>
-                    <h2 className="inv-col-head">Thanked</h2>
+            <div className="table-form">
+                <div className="inventory-wrapper">
+                    <div className="inventory-header">
+                        <h2 className="inv-col-head">Donor</h2>
+                        <h2 className="inv-col-head">Items Donated</h2>
+                        <h2 className="inv-col-head">Item Type</h2>
+                        <h2 className="inv-col-head">Amount</h2>
+                        <h2 className="inv-col-head">Date Donated</h2>
+                        <h2 className="inv-col-head">Thanked</h2>
+                    </div>
+                    <InventoryEntry donor = "surbhu" item_donated = "crocs" item_type = "shoes" amount = "5" date = "march 4"/>
+                    <InventoryEntry donor = "surbhu" item_donated = "crocs" item_type = "shoes" amount = "5" date = "march 4"/>
                 </div>
-                <InventoryEntry donor = "surbhu" item_donated = "crocs" item_type = "shoes" amount = "5" date = "march 4"/>
-                <InventoryEntry donor = "surbhu" item_donated = "crocs" item_type = "shoes" amount = "5" date = "march 4"/>
+                <div className="create-form">
+                    {visible && <EntryPopup />}
+                </div>
             </div>
         </div>
     )
