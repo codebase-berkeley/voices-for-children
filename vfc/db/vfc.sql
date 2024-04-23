ee020144b51f9c7e7aa426596f9dfc43184b585e
Sasha Singh <sashashruti2004@gmail.com>
2024-04-23 15:02:35 -0700

    postman

--- vfc/db/vfc.sql
+++ vfc/db/vfc.sql
@@ -23,7 +23,7 @@ CREATE TABLE inkindDonations
 );
 
 INSERT INTO players(name, score) VALUES ('player 1', 0);
-INSERT INTO inkindDonations(donor, itemsDonated, itemType, amount, dateDonated, thanked) VALUES ('sasha', 'legos', 'toys', 10, DATE '2015-12-23', 'thanked sasha by giving her nothing');
+INSERT INTO inkindDonations(donor, itemsDonated, itemType, amount, dateDonated, thanked) VALUES ('sasha', 'legos', 'toys', 10, DATE '2015-12-23', 'thanked sasha by giving her flowers - sunflowers');
 -- INSERT INTO inkindDonations(donor, itemsDonated, itemType, amount, dateDonated, thanked) VALUES ('ant', 'hot wheels', 'toys', DATE '2015-12-23', 'thanked ant by giving him nothing');
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;
