4a69ce397950c853e054cccc72aad8f7cf9932a6
Angie <angiezhu05@gmail.com>
2024-05-02 00:16:13 -0700

    working delete endpoint

--- vfc/src/pages/api/deleteInventory.js
+++ vfc/src/pages/api/deleteInventory.js
@@ -4,30 +4,7 @@ const { Pool } = require("pg");
 const pool = new Pool({ database: "vfc" });
 
 export default async function deleteInventory(req, res) {
-  console.log("Request method:", req.method);
-  console.log("Request body:", req.body);
 
-  if (req.method === "DELETE") {
-    try {
-      const itemId = req.body.itemId;
-      console.log("Deleting item with ID:", itemId);
-
-      const query = await pool.query(
-        `DELETE FROM inkindDonations WHERE id = $1;`,
-        [itemId]
-      );
-
-      console.log("Deletion result:", query.rows);
-      res.status(200).json(query.rows);
-    } catch (error) {
-      console.error("Error executing query", error);
-      res.status(500).send("Internal Server Error");
-    }
-  } else {
-    console.log("Invalid request method");
-    res.status(405).send("Method Not Allowed");
-  console.log("inside endpoint", req.body);
-  
   if (req.method === "DELETE") {
     try {
       const itemId = req.body.itemId; // Correctly access itemId from req.body
