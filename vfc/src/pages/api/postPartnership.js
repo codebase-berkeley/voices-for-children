ac648b26e5d027d3165df22292275fd2083603ab
Justin Chen <justinchen722@berkeley.edu>
2024-05-02 00:58:42 -0700

    finally finihsed edits past

--- vfc/src/pages/api/postPartnership.js
+++ vfc/src/pages/api/postPartnership.js
@@ -1,42 +1,39 @@
-import nextConnect from "next-connect";
-import multer from "multer";
-import { Pool } from "pg";
 
+const { Pool } = require("pg");
 const pool = new Pool({ database: "vfc" });
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+export default async function getPartnership(req, res) {
+  if (req.method === "POST") {
+    try {
+      const {
+        name,
+        location,
+        citystate,
+        gifttype,
+        date,
+        email,
+        poc,
+        phone,
+        link,
+        image,
+      } = req.body;
 
-const handler = nextConnect();
+      const imageData = image || "";
+      
+      const query = await pool.query(
+        `INSERT INTO communityPartnerships (name, location, citystate, gifttype, date, email, poc, phone, link, image) 
+                                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id;`,
+        [name, location, citystate, gifttype, date, email, poc, phone, link, imageData]
+      );
 
-handler.use(upload.single("image"));
+      const id = query.rows[0].id;
 
-handler.post(async (req, res) => {
-  const { companyName, location, cityState, giftType, date, email, poc, phone, link } = req.body;
-  const file = req.file; // The image file
-
-  try {
-    const base64Image = file ? `data:image/${file.mimetype.split('/')[1]};base64,${file.buffer.toString('base64')}` : null;
-
-    const query = await pool.query(
-      `INSERT INTO communityPartnerships (name, location, citystate, gifttype, date, email, poc, phone, link, image) 
-       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id;`,
-      [companyName, location, cityState, giftType, date, email, poc, phone, link, file ? file.buffer : null]
-    );
-
-    const id = query.rows[0].id;
-    res.status(201).json({ id: id, image: base64Image });
-  } catch (error) {
-    console.error("error executing query", error);
-    res.status(500).send("Internal Server Error");
+      res.send({ id: id });
+    } catch (error) {
+      console.error("error executing query", error);
+      res.status(500).send("Internal Server Error");
+    }
+  } else {
+    res.status(405).end(); // Method Not Allowed
   }
-});
-
-
-export const config = {
-  api: {
-    bodyParser: false, // Disable body parsing; multer will handle it
-  },
-};
-
-export default handler;
+}
