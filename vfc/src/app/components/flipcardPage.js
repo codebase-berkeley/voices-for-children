3aa8c811fd8abdd76d2447a8a7a860a71cb7a0ee
Cindy Sanchez <cindysanchez@berkeley.edu>
2024-05-02 01:14:16 -0700

    old filter

--- vfc/src/app/components/flipcardPage.js
+++ vfc/src/app/components/flipcardPage.js
@@ -13,24 +13,38 @@ function MultipleFlipCards(props) {
   };
 
   // Filters through data from filters checked
+  // const filteredData = props.data.filter((card) => {
+  //   //changes 07/22/2005 to July 22, 2005
+  //   const newDate = formatDate(card.date).toLowerCase();
+  //   // If there are no filters selected, show everything
+  //   if (props.filters.length == 0) {
+  //     return true;
+  //     // Else return all items that have an attribute present in the currFilter array
+  //   } else {
+  //     return (
+  //       props.filters.includes(card.gifttype.toLowerCase()) ||
+  //       //filter ex. Given San Diego, CA it will include when checking off San Diego box
+  //       props.filters.every((filter) =>
+  //         card.citystate.toLowerCase().includes(filter.toLowerCase())
+  //       ) ||
+  //       props.filters.every((filter) => newDate.includes(filter.toLowerCase()))
+  //     );
+  //   }
+  // });
   const filteredData = props.data.filter((card) => {
-    //changes 07/22/2005 to July 22, 2005
     const newDate = formatDate(card.date).toLowerCase();
-    // If there are no filters selected, show everything
-    if (props.filters.length == 0) {
-      return true;
-      // Else return all items that have an attribute present in the currFilter array
-    } else {
-      return (
-        props.filters.includes(card.gifttype.toLowerCase()) ||
-        //filter ex. Given San Diego, CA it will include when checking off San Diego box
-        props.filters.every((filter) =>
-          card.citystate.toLowerCase().includes(filter.toLowerCase())
-        ) ||
-        props.filters.every((filter) => newDate.includes(filter.toLowerCase()))
-      );
-    }
-  });
+    const cardAttributes = [
+        card.gifttype.toLowerCase(),
+        card.citystate.toLowerCase(),
+        newDate
+    ];
+
+    // Return true if there are no filters, else check each filter on all relevant attributes
+    return props.filters.length === 0 || props.filters.every((filter) => {
+        const lowerCaseFilter = filter.toLowerCase();
+        return cardAttributes.some(attribute => attribute.includes(lowerCaseFilter));
+    });
+});
   // Filters through data by searching
   const searchedData = filteredData.filter((card) => {
     //if nothing in the input, return everything
