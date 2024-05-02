203124d53650c872590d1cf2afbf12fa7f8aa489
Dominic  Charles <dominiccharles@Dominics-MacBook-Pro.local>
2024-05-01 21:23:10 -0700

    filterobject

--- vfc/src/app/components/flipcardPage.js
+++ vfc/src/app/components/flipcardPage.js
@@ -14,23 +14,21 @@ function MultipleFlipCards(props) {
 
   // Filters through data from filters checked
   const filteredData = props.data.filter((card) => {
-    //changes 07/22/2005 to July 22, 2005
     const newDate = formatDate(card.date).toLowerCase();
-    // If there are no filters selected, show everything
-    if (props.filters.length == 0) {
-      return true;
-      // Else return all items that have an attribute present in the currFilter array
-    } else {
-      return (
-        props.filters.includes(card.gifttype.toLowerCase()) ||
-        //filter ex. Given San Diego, CA it will include when checking off San Diego box
-        props.filters.every((filter) =>
-          card.citystate.toLowerCase().includes(filter.toLowerCase())
-        ) ||
-        props.filters.every((filter) => newDate.includes(filter.toLowerCase()))
-      );
-    }
-  });
+    const cardAttributes = [
+        card.gifttype.toLowerCase(),
+        card.citystate.toLowerCase(),
+        newDate
+    ];
+
+    // Return true if there are no filters, else check each filter on all relevant attributes
+    return props.filters.length === 0 || props.filters.every((filter) => {
+      console.log(filter);
+        const lowerCaseFilter = filter.filtername.toLowerCase();
+        console.log("lowere case filter", lowerCaseFilter);
+        return cardAttributes.some(attribute => attribute.includes(lowerCaseFilter));
+    });
+});
   // Filters through data by searching
   const searchedData = filteredData.filter((card) => {
     //if nothing in the input, return everything
@@ -46,7 +44,6 @@ function MultipleFlipCards(props) {
       <div className="card-container">
         {searchedData.map((card, index) => (
           <FlipCard
-            id={card.id}
             key={index}
             name={card.name}
             location={card.location}
@@ -58,8 +55,6 @@ function MultipleFlipCards(props) {
             date={card.date}
             giftType={card.gifttype}
             link={card.link}
-            data={props.data}
-            setData={props.setData}
           />
         ))}
       </div>
