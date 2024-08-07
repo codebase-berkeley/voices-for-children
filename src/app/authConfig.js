53c84b42131975aac687d55b0e84e52fff132156
Anthony <anthonylu@berkeley.edu>
2024-08-07 16:17:23 -0700

    fix authConfig

--- src/app/authConfig.js
+++ src/app/authConfig.js
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { LogLevel } from "@azure/msal-browser";
+import { LogLevel } from '@azure/msal-browser';
 
 /**
  * Enter here the user flows and custom policies for your B2C application
@@ -11,32 +11,27 @@ import { LogLevel } from "@azure/msal-browser";
  * To learn more about custom policies, visit: https://docs.microsoft.com/en-us/azure/active-directory-b2c/custom-policy-overview
  */
 
-const clientId = "86a9a461-817e-4735-8766-edfac1beedd4";
-const tenantName = "vfcspeakupnow";
+const clientId = '86a9a461-817e-4735-8766-edfac1beedd4';
+const tenantName = 'Voices for Children';
 
 const flows = {
-  localAccSignIn: "B2C_1_Main_Flow_VFC", // e.g., B2C_1_main_flow
-  msid: "B2C_1_msid",
-};
+    localAccSignIn: 'B2C_1_main_flow', //e.g., B2C_1_main_flow
+    msid: 'B2C_1_msid',
+}
 
 const activeFlow = flows.localAccSignIn;
 
 export const b2cPolicies = {
-  names: {
-    signUpSignIn: activeFlow,
-  },
-  authorities: {
-    signUpSignIn: {
-      authority: `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/${activeFlow}`,
+    names: {
+        signUpSignIn: activeFlow
     },
-  },
-  authorityDomain: `${tenantName}.b2clogin.com`,
-  authorityMetadata:
-    '{"token_endpoint": "https://login.microsoftonline.com/common/oauth2/v2.0/token", "token_endpoint_auth_methods_supported": ["client_secret_post", "private_key_jwt", "client_secret_basic"], "jwks_uri": "https://login.microsoftonline.com/common/discovery/v2.0/keys", "response_modes_supported": ["query", "fragment", "form_post"], "subject_types_supported": ["pairwise"], "id_token_signing_alg_values_supported": ["RS256"], "response_types_supported": ["code", "id_token", "code id_token", "id_token token"], "scopes_supported": ["' +
-    clientId +
-    '", "openid", "profile", "email", "offline_access"], "issuer": "https://login.microsoftonline.com/' +
-    tenantName +
-    '/v2.0", "request_uri_parameter_supported": false, "userinfo_endpoint": "https://graph.microsoft.com/oidc/userinfo", "authorization_endpoint": "https://login.microsoftonline.com/common/oauth2/v2.0/authorize", "device_authorization_endpoint": "https://login.microsoftonline.com/common/oauth2/v2.0/devicecode", "http_logout_supported": true, "frontchannel_logout_supported": true, "end_session_endpoint": "https://login.microsoftonline.com/common/oauth2/v2.0/logout", "claims_supported": ["sub", "iss", "cloud_instance_name", "cloud_instance_host_name", "cloud_graph_host_name", "msgraph_host", "aud", "exp", "iat", "auth_time", "acr", "nonce", "preferred_username", "name", "tid", "ver", "at_hash", "c_hash", "email"], "kerberos_endpoint": "https://login.microsoftonline.com/common/kerberos", "tenant_region_scope": null, "cloud_instance_name": "microsoftonline.com", "cloud_graph_host_name": "graph.windows.net", "msgraph_host": "graph.microsoft.com", "rbac_url": "https://pas.windows.net"}',
+    authorities: {
+        signUpSignIn: {
+            authority: `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/${activeFlow}`,
+        }
+    },
+    authorityDomain: `${tenantName}.b2clogin.com`,
+    authorityMetadata: '{"token_endpoint": "https://login.microsoftonline.com/common/oauth2/v2.0/token", "token_endpoint_auth_methods_supported": ["client_secret_post", "private_key_jwt", "client_secret_basic"], "jwks_uri": "https://login.microsoftonline.com/common/discovery/v2.0/keys", "response_modes_supported": ["query", "fragment", "form_post"], "subject_types_supported": ["pairwise"], "id_token_signing_alg_values_supported": ["RS256"], "response_types_supported": ["code", "id_token", "code id_token", "id_token token"], "scopes_supported": ["' + clientId + '", "openid", "profile", "email", "offline_access"], "issuer": "https://login.microsoftonline.com/' + tenantName + '/v2.0", "request_uri_parameter_supported": false, "userinfo_endpoint": "https://graph.microsoft.com/oidc/userinfo", "authorization_endpoint": "https://login.microsoftonline.com/common/oauth2/v2.0/authorize", "device_authorization_endpoint": "https://login.microsoftonline.com/common/oauth2/v2.0/devicecode", "http_logout_supported": true, "frontchannel_logout_supported": true, "end_session_endpoint": "https://login.microsoftonline.com/common/oauth2/v2.0/logout", "claims_supported": ["sub", "iss", "cloud_instance_name", "cloud_instance_host_name", "cloud_graph_host_name", "msgraph_host", "aud", "exp", "iat", "auth_time", "acr", "nonce", "preferred_username", "name", "tid", "ver", "at_hash", "c_hash", "email"], "kerberos_endpoint": "https://login.microsoftonline.com/common/kerberos", "tenant_region_scope": null, "cloud_instance_name": "microsoftonline.com", "cloud_graph_host_name": "graph.windows.net", "msgraph_host": "graph.microsoft.com", "rbac_url": "https://pas.windows.net"}'
 };
 
 /**
@@ -45,45 +40,46 @@ export const b2cPolicies = {
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
  */
 export const msalConfig = {
-  auth: {
-    clientId: clientId, // This is the ONLY mandatory field that you need to supply.
-    authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose SUSI as your default authority.
-    knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
-    redirectUri: "https://voices-for-children-cb-2b944e8d4187.herokuapp.com/donation", // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
-    postLogoutRedirectUri: "https://voices-for-children-cb-2b944e8d4187.herokuapp.com/", // Indicates the page to navigate after logout.
-    navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
-    authorityMetadata: b2cPolicies.authorityMetadata,
-  },
-  cache: {
-    cacheLocation: "sessionStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
-    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-  },
-  system: {
-    loggerOptions: {
-      loggerCallback: (level, message, containsPii) => {
-        if (containsPii) {
-          return;
-        }
-        switch (level) {
-          case LogLevel.Error:
-            console.error(message);
-            return;
-          case LogLevel.Info:
-            console.info(message);
-            return;
-          case LogLevel.Verbose:
-            console.debug(message);
-            return;
-          case LogLevel.Warning:
-            console.warn(message);
-            return;
-          default:
-            return;
-        }
-      },
+    auth: {
+        clientId: clientId, // This is the ONLY mandatory field that you need to supply.
+        authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose SUSI as your default authority.
+        knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
+        redirectUri: 'http://localhost:3000/donation', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
+        postLogoutRedirectUri: 'http://localhost:3000/', // Indicates the page to navigate after logout.
+        navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
+        authorityMetadata: b2cPolicies.authorityMetadata
     },
-  },
+    cache: {
+        cacheLocation: 'sessionStorage', // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {
+                    return;
+                }
+                switch (level) {
+                    case LogLevel.Error:
+                        console.error(message);
+                        return;
+                    case LogLevel.Info:
+                        console.info(message);
+                        return;
+                    case LogLevel.Verbose:
+                        console.debug(message);
+                        return;
+                    case LogLevel.Warning:
+                        console.warn(message);
+                        return;
+                    default:
+                        return;
+                }
+            },
+        },
+    },
 };
+
 
 /**
  * Scopes you add here will be prompted for user consent during sign-in.
@@ -93,7 +89,5 @@ export const msalConfig = {
  */
 
 export const authScopes = {
-  scopes: [
-    "https://vfcspeakupnow.onmicrosoft.com/86a9a461-817e-4735-8766-edfac1beedd4/Files.Read",
-  ],
+    scopes: ['https://vfcspeakupnow.onmicrosoft.com/86a9a461-817e-4735-8766-edfac1beedd4/Files.Read']
 };
