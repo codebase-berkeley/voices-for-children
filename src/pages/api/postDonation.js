aa50fcf65df730b3a6d733623e8a10f354b9fcb8
Sasha Singh <sashashruti2004@gmail.com>
2024-08-22 10:37:11 -0700

    modify endpoint

--- src/pages/api/postDonation.js
+++ src/pages/api/postDonation.js
@@ -1,26 +1,44 @@
-const { Pool } = require("pg");
-const pool = new Pool({ database: "vfc" });
+// const { Pool } = require("pg");
+// const pool = new Pool({ database: "vfc" });
+
+// export default async function postDonation(req, res) {
+//   try {
+//     const body = req.body;
+//     const query = await pool.query(`INSERT INTO inkindDonations (donor, item_donated, item_type, amount, date, thanked, stock)
+//     VALUES ($1, $2, $3, $4, $5, $6, $7);`,
+//     [
+//         body.donor,
+//         body.name,
+//         body.type,
+//         body.amt,
+//         body.date,
+//         body.desc,
+//         body.stock,
+//     ]
+//     );
+
+//     res.status(200).json({ success: true, message: 'Donation added successfully.' });
+//   } catch (error) {
+//     console.error("Error executing query", error.stack);
+//     res.status(500).json({ success: false, message: 'Error adding donation.', error: error.message });
+//   }
+// }
 
 export default async function postDonation(req, res) {
-  try { 
+  try {
     const body = req.body;
-    const query = await pool.query(`INSERT INTO inkindDonations (donor, item_donated, item_type, amount, date, thanked, stock) 
-    VALUES ($1, $2, $3, $4, $5, $6, $7);`,
-    [
-        body.donor,
-        body.name,
-        body.type,
-        body.amt,
-        body.date,
-        body.desc,
-        body.stock,
-    ]
+    const query = await pool.query(
+      `INSERT INTO players(name, score) VALUES ($1, 0);`
     );
-
-    res.status(200).json({ success: true, message: 'Donation added successfully.' });
+    res.status(200).json({ success: true, message: "successful test query" });
   } catch (error) {
     console.error("Error executing query", error.stack);
-    res.status(500).json({ success: false, message: 'Error adding donation.', error: error.message });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "not successful test query",
+        error: error.message,
+      });
   }
 }
-
