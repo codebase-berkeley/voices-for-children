4a8c721ed98bbaacbe54f1bf47af935a2fb79972
Sasha Singh <sashashruti2004@gmail.com>
2024-08-25 20:37:53 -0700

    wip

--- src/pages/api/getPartnership.js
+++ src/pages/api/getPartnership.js
@@ -4,6 +4,9 @@ import json from "./communitydata.json";
 
 export default async function getPartnership(req, res) {
   try {
+    let json = JSON.parse(
+      fs.readFileSync("src/pages/api/communitydata.json", "utf-8")
+    );
     res.send(json);
   } catch (error) {
     console.error("error excuting query", error);
